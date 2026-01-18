c2588e4ec5103832b32c9da047f4958f3210074c
Pragya Dhungel <pragyadhungel@Pragyas-MacBook-Air.local>
2026-01-18 10:07:49 +0545

    added dictionary

---
+++ dict.py
@@ -0,0 +1,10 @@
+#store the following word meaning in python dictionary
+# table : "a piece of furniture", "list of facts and figures"
+# cat : "a small animal"
+
+dictionary = {
+    "table" : ["a piece of furniture"," list of facts and figures"],
+     "cat" : "a small animal",
+}
+#dictionary.update({"table" : "I'm a table"})
+print(dictionary.get("table"))
